b24bd85415a94e76a75fd435070ac5e902927bc0
jeffery <jeffery9@outlook.com>
2023-03-21 11:52:57 +0800

    feature: records

---
+++ src/controllers/recordController.js
@@ -0,0 +1,18 @@
+const recordService = require("../services/recordService");
+
+
+const getRecordForWorkout = (req, res) => {
+    const {
+        params: { workoutId },
+      } = req;
+
+    // console.log(req);
+    const allRecordForWorkout = recordService.getRecordForWorkout(workoutId);
+    res.send( { status: "OK", data: allRecordForWorkout } );
+};
+
+
+
+module.exports=({
+    getRecordForWorkout
+}) ;
--- src/v1/routes/workoutRoutes.js
+++ src/v1/routes/workoutRoutes.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const workoutController = require("../../controllers/workoutController");
+const recordController = require("../../controllers/recordController");
+
 
 
 const router = express.Router();
@@ -7,6 +9,9 @@ const router = express.Router();
 router.get("/", workoutController.getAllWorkouts);
 
 router.get("/:workoutId", workoutController.getOneWorkout);
+
+router.get("/:workoutId/records", recordController.getRecordForWorkout);
+
 
 router.post("/", workoutController.createNewWorkout);
 
