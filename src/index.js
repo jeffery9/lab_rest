384ec5c3037abd4e3838448fa9ae91b791fda514
jeffery <jeffery9@outlook.com>
2023-03-21 10:41:37 +0800

    format json in body

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const express = require("express"); 
+const bodyParser = require("body-parser");
+
+
 const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
 
 
 const app = express(); 
 const PORT = process.env.PORT || 3000; 
+
+app.use(bodyParser.json());
 
 // For testing purposes 
 app.get("/", (req, res) => { 
